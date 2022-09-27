6d8ad5a445d5706dae8a95d5e1267b288682ee6e
AtulJoshyy <atuljoshyy@gmail.com>
2022-09-27 09:11:42 +0530

    landing page done ✅

--- pages/books/atomichabits.js
+++ pages/books/atomichabits.js
@@ -26,7 +26,7 @@ const atomichabits = () => {
 
         If you remove the cue, your brain is not activated.
         If you remove the craving, you have no need to act.
-        If the response is too hard, you won’t be able to do it or won’t try.
+        If the response is too hard, you won&apos;t be able to do it or won&apos;t try.
         If the reward is not satisfying, you have no reason to want to attain it again.
         You can use this information to form or break habits by altering the various stages to your advantage. Each stage encompasses a law that guides you in that endeavor.
 
@@ -34,10 +34,10 @@ const atomichabits = () => {
         Think of these laws as a framework for designing each stage for optimal habit formation.
 
         Cue: Make It Obvious
-        Because habits are automatic behaviors, you likely don’t notice every cue triggering you to act. Therefore, the first step in creating cues that lead to good behaviors is to become aware of them.
+        Because habits are automatic behaviors, you likely don&apos;t notice every cue triggering you to act. Therefore, the first step in creating cues that lead to good behaviors is to become aware of them.
 
         Become Aware of Habits
-        Make a habit scorecard to list all your current habits performed daily. Because behaviors influence each other, the end of one habit often serves as a cue for another. When you list your habits, you’re able to see which actions precede them and which follow. By listing your cues and rewards in this way, you will see what current behaviors may be suitable to cue new desired behaviors.
+        Make a habit scorecard to list all your current habits performed daily. Because behaviors influence each other, the end of one habit often serves as a cue for another. When you list your habits, you&apos;re able to see which actions precede them and which follow. By listing your cues and rewards in this way, you will see what current behaviors may be suitable to cue new desired behaviors.
 
         Use Awareness to Your Advantage
         You can exploit the habit scorecard in two ways to form better habits.
@@ -62,21 +62,21 @@ const atomichabits = () => {
         Craving: Make It Attractive
         For your cravings to lead to action, the possible reward must be attractive. Cravings involve the sensations of wanting—the anticipation of pleasure—and liking—the experience of pleasure. Both sensations trigger the production of dopamine in the brain, or the chemical involved in desire, but 90% of dopamine production is dedicated to wanting. The anticipation of something outweighs the pleasure of receiving that thing.
 
-        Think about how much more excited you are before a date than when on the date. If it’s a first date, you anticipate making a connection, which excites you. If, when you’re on the date, you have a good time but don’t feel a strong connection, you won’t want to continue dating that person.
-        However, if you feel a connection, you will crave a second date because you desire to feel that connection again. It’s the promise of continued connection that keeps you motivated to date this person.
+        Think about how much more excited you are before a date than when on the date. If it&apos;s a first date, you anticipate making a connection, which excites you. If, when you&apos;re on the date, you have a good time but don&apos;t feel a strong connection, you won&apos;t want to continue dating that person.
+        However, if you feel a connection, you will crave a second date because you desire to feel that connection again. It&apos;s the promise of continued connection that keeps you motivated to date this person.
         If anticipation is the greatest craving, you must create more promise for the rewards your actions lead to. There are two ways that rewards can be heightened to create stronger desires.
 
         Heightened Rewards
         Temptation bundling is one way to make any behavior more attractive. Like habit stacking, use the end of a new behavior as a cue for something that excites you. This helps make the new behavior more enticing.
 
-        You’ve set up the end of lunch as a cue to walk for 20 minutes, but you don’t feel driven to walk because you’d rather play video games. If playing video games for 20 minutes becomes the reward for walking, you’ll begin to crave the walk so you can claim your reward afterward.
+        You&apos;ve set up the end of lunch as a cue to walk for 20 minutes, but you don&apos;t feel driven to walk because you&apos;d rather play video games. If playing video games for 20 minutes becomes the reward for walking, you&apos;ll begin to crave the walk so you can claim your reward afterward.
         The formula is: After X [current habit], I will do Y [new habit]. After I do Y, I get to do Z [craved habit].”
-        Redefining your behaviors can also make them more appealing. Rather than thinking about having to walk 20 minutes a day, change the perspective to getting to walk 20 minutes a day. This small change in perspective highlights the positive elements of walking. You begin to see how fortunate you are to have the ability to walk and the time in your day to do so. When you feel gratitude for the activities you’re able to participate in, you’ll be more driven to keep participating.
+        Redefining your behaviors can also make them more appealing. Rather than thinking about having to walk 20 minutes a day, change the perspective to getting to walk 20 minutes a day. This small change in perspective highlights the positive elements of walking. You begin to see how fortunate you are to have the ability to walk and the time in your day to do so. When you feel gratitude for the activities you&apos;re able to participate in, you&apos;ll be more driven to keep participating.
 
         Response: Make It Easy
         You will only follow through on behaviors that are easy to perform and require little effort. That is simply human nature. Therefore, to stay motivated, make behavior as effortless as possible.
 
-        But making behaviors easy doesn’t mean only doing easy things. The idea is to make it easy for you to keep showing up for the behavior you want to perform. By simply showing up, you maintain your desired identity, which gives you pride and confidence to keep making progress.
+        But making behaviors easy doesn&apos;t mean only doing easy things. The idea is to make it easy for you to keep showing up for the behavior you want to perform. By simply showing up, you maintain your desired identity, which gives you pride and confidence to keep making progress.
 
         Making Behaviors Easier
         Reducing the effort of an action means removing the friction existing between you and the behavior. The more friction there is, the less likely you are to act.
@@ -85,36 +85,36 @@ const atomichabits = () => {
         If you want to read more before bed, place a book on your pillow after waking up each morning.
         The two-minute rule is also effective in making behaviors easier. Often, you jump into the biggest changes required when trying to build new habits or start new behaviors. But big changes in behavior are hard to maintain over time. You will make more progress if you break the behavior down into tiny two-minute increments. These increments will build into massive achievements.
 
-        You want to start jogging and decide to jog for 20 minutes each day after work. You are successful for a few days, but on the fourth day, you’re too tired, so you skip it. Suddenly, the behavior has stopped.
-        However, you likely have enough energy to lay out your running clothes, and it only takes two minutes. Or better yet, you’ve laid out running clothes before leaving for work to reduce friction. The next step is to put them on. The next step after that is to put on your shoes. Then, leave the house. Then, walk to the sidewalk. Each of these two-minute actions connect to get you to the point of being ready to run. The obstacles to overcome are small compared with the overwhelming idea of running for 20 minutes.
+        You want to start jogging and decide to jog for 20 minutes each day after work. You are successful for a few days, but on the fourth day, you&apos;re too tired, so you skip it. Suddenly, the behavior has stopped.
+        However, you likely have enough energy to lay out your running clothes, and it only takes two minutes. Or better yet, you&apos;ve laid out running clothes before leaving for work to reduce friction. The next step is to put them on. The next step after that is to put on your shoes. Then, leave the house. Then, walk to the sidewalk. Each of these two-minute actions connect to get you to the point of being ready to run. The obstacles to overcome are small compared with the overwhelming idea of running for 20 minutes.
         Some days, you may only get as far as changing into the clothes, but that small act is still a vote cast for your identity as a jogger and is still considered a victory.
         Small successes motivate you to achieve others. Commit to two-minute actions at a time, and it will be easier to perform each required step until you have achieved the full habit.
 
         Reward: Make It Satisfying
-        If the end result of your efforts is unenjoyable, why would you want to do them again? You wouldn’t, which is why rewards need to be satisfying for habits to form.
+        If the end result of your efforts is unenjoyable, why would you want to do them again? You wouldn&apos;t, which is why rewards need to be satisfying for habits to form.
 
         Many rewards you receive are delayed. You only receive a paycheck after weeks of work. You only get a final grade after months of studying. But human nature is wired to want instant gratification, and most good behaviors need time before the positive results accumulate. You sacrifice now to benefit later. To stay motivated to continue good habits, find ways to create rewards that are instantly satisfying.
 
         Adding Instant Gratification to Rewards
         Reinforcements are good motivators for continued action. You remember the end of behavior more than any other part. Therefore, make the end of your behavior rewarding with a reinforcement to motivate continued action.
 
-        You won’t notice a major difference in your savings account after a week if you deposit one dollar a day. The reward of more financial freedom is too far down the road to elicit immediate pleasure. But the act of saving a dollar a day can be rewarding with a small reinforcement. Each time you deposit a dollar into your savings, give yourself an hour of relaxation, a cookie, or an extra 30 minutes of television. You’ll feel motivated to keep saving if you end the action with something gratifying.
+        You won&apos;t notice a major difference in your savings account after a week if you deposit one dollar a day. The reward of more financial freedom is too far down the road to elicit immediate pleasure. But the act of saving a dollar a day can be rewarding with a small reinforcement. Each time you deposit a dollar into your savings, give yourself an hour of relaxation, a cookie, or an extra 30 minutes of television. You&apos;ll feel motivated to keep saving if you end the action with something gratifying.
         Whatever the reinforcement is, it must be satisfying and instantly received.
-        Habit tracking can also make behaviors more rewarding. Create visual representations of your progress by marking a day on a calendar, transferring pebbles from one jar to another, or keeping a journal log of your completed actions. When you can visually see your accomplishments, you’ll be motivated to continue acting.
+        Habit tracking can also make behaviors more rewarding. Create visual representations of your progress by marking a day on a calendar, transferring pebbles from one jar to another, or keeping a journal log of your completed actions. When you can visually see your accomplishments, you&apos;ll be motivated to continue acting.
 
         The act of tracking can feel rewarding in and of itself. It is satisfying to mark each successful completion of an action in some way. The pleasure experienced through that act becomes a cue to want to feel that satisfaction again.
 
         Only track one major habit at a time to avoid becoming overwhelmed by the act.
 
         Breaking Bad Habits
-        You likely have habits you wish you didn’t. To break bad habits, invert the laws from positive to negative in the following way:
+        You likely have habits you wish you didn&apos;t. To break bad habits, invert the laws from positive to negative in the following way:
 
         Cue—Make it invisible.
         Craving—Make it unattractive.
         Response—Make it difficult.
         Reward—Make it unsatisfying.
         Finding the Right Habits
-        The habits you gravitate toward and are able to maintain are influenced by your genetic make-up, predispositions, and natural talents. Choose behaviors that highlight your strengths and interests—they’ll be more enjoyable and easier to stick with.
+        The habits you gravitate toward and are able to maintain are influenced by your genetic make-up, predispositions, and natural talents. Choose behaviors that highlight your strengths and interests—they&apos;ll be more enjoyable and easier to stick with.
 
         The Big 5 Personality Traits
         To figure out which behaviors are optimal for you, learn which personality traits you possess.
@@ -126,7 +126,7 @@ const atomichabits = () => {
         Extroversion—from sociable and gregarious to reclusive and reticent.
         Agreeableness—from affable and caring to difficult and withdrawn.
         Neuroticism—from fretful and sensitive to assured and resilient.
-        Your personality doesn’t dictate which behaviors you’re capable of performing. However, your personality does suggest which behaviors you will most likely be successful with.
+        Your personality doesn&apos;t dictate which behaviors you&apos;re capable of performing. However, your personality does suggest which behaviors you will most likely be successful with.
 
         A less agreeable person will struggle to build a habit of forming one social connection a week or sending out greeting cards.
         An extrovert will have a harder time staying off of social media than an introvert.
@@ -134,19 +134,19 @@ const atomichabits = () => {
         There is a version of each habit and behavior that falls along your spectrum of personality. Choose the version that fits your natural personality, not the ones that friends, family, or society deem best.
 
         Your friend lost weight by working out at a gym. But you hate crowds, so taking daily walks or hiking works better for you.
-        Your mother saved money by removing lattes and takeout from her daily routine. But you enjoy lattes and don’t cook, so putting 10% of your paycheck in a savings account works better for you.
+        Your mother saved money by removing lattes and takeout from her daily routine. But you enjoy lattes and don&apos;t cook, so putting 10% of your paycheck in a savings account works better for you.
         Stick to what works for you to experience the greatest success.
 
         Continuing to Show Up
         Boredom is inevitable with any repeated activity; therefore, there will be a point in which your excitement and motivation for a new behavior wanes. When this happens, it is easy to abandon good behaviors that are still working to find behaviors that are novel and exciting. To curb this harmful instinct, keep behaviors exciting by following the Goldilocks Rule, building on momentum, and crafting a flexible identity.
 
         The Goldilocks Rule
-        The brain loves a challenge, but only when that challenge is within your capabilities. If the challenge is too easy, you lose interest. If it’s too hard, you become frustrated or disheartened. A challenge that is just right keeps you engaged and motivated to improve. This is the essence of the Goldilocks Rule, also referred to as just manageable difficulty.
+        The brain loves a challenge, but only when that challenge is within your capabilities. If the challenge is too easy, you lose interest. If it&apos;s too hard, you become frustrated or disheartened. A challenge that is just right keeps you engaged and motivated to improve. This is the essence of the Goldilocks Rule, also referred to as just manageable difficulty.
 
         There is a balance required between increasing the difficulty of a task just enough to stay interesting and keeping the level of difficulty at a point where success can still be attained. The just manageable difficulty meter sits at the edge of your current abilities. When each attempt to act includes a 50/50 chance of success, you will continue to work hard because you crave the satisfaction of succeeding.
 
         Building on Momentum
-        Another reason you lose motivation is that you’ve reached the habit line of your current behavior, or the point where the action becomes automated. If you stop there, you will stop growing and lose motivation. You may think you’re making progress because you’re putting in the reps, but all you’re doing is reinforcing the same behavior. If your actions don’t change, your results won’t change.
+        Another reason you lose motivation is that you&apos;ve reached the habit line of your current behavior, or the point where the action becomes automated. If you stop there, you will stop growing and lose motivation. You may think you&apos;re making progress because you&apos;re putting in the reps, but all you&apos;re doing is reinforcing the same behavior. If your actions don&apos;t change, your results won&apos;t change.
 
         Reaching a goal requires momentum within your system. Once a habit becomes automated, build on it with 1% improvements to keep behaviors novel and progress continuous.
 
@@ -157,8 +157,8 @@ const atomichabits = () => {
         If you label yourself vegan and have to start eating animal protein for your health, what is your new identity?
         The solution is to think of yourself in terms of the associated characteristics to avoid losing your identity if that label changes. When you identify with positive characteristics, rather than one positive label, your identity is flexible.
 
-        “I’m a CEO” becomes “I’m someone who is smart, innovative, and a strong leader.” You can still be those things in a new position.
-        “I’m a vegan” becomes “I’m someone who is conscientious about what I eat and consumes foods that are healthy for me and the earth.” You can still have positive eating habits regardless of your dietary requirements.</p>
+        “I&apos;m a CEO” becomes “I&apos;m someone who is smart, innovative, and a strong leader.” You can still be those things in a new position.
+        “I&apos;m a vegan” becomes “I&apos;m someone who is conscientious about what I eat and consumes foods that are healthy for me and the earth.” You can still have positive eating habits regardless of your dietary requirements.</p>
     </div>
   )
 }
